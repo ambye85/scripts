3e34fc199f50c5f3ff6656e2927034f57b31ed08
Ashley Bye <ashley.bye85@gmail.com>
2019-12-19 07:59:47 +0000

    Use gruvbox theme

--- zsh.sh
+++ zsh.sh
@@ -4,3 +4,12 @@
 # ZSH
 ################################################################################
 brew install zsh zsh-completions
+
+cp ~/.dotfiles/zshrc ~/.zshrc
+#cp ~/.dotfiles/p10k.zsh ~/.p10k.zsh
+
+# install zplugin
+mkdir ~/.zplugin
+git clone https://github.com/zdharma/zplugin.git ~/.zplugin/bin
+source ~/.zshrc
+zplugin self-update
