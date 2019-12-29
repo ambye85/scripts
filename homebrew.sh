597dfe602a5ab2d32d5c6cb2e5982bd03fb8b2ad
Ashley Bye <ashley.bye85@gmail.com>
2019-12-29 21:05:10 +0000

    Add useful brew formulae

--- homebrew.sh
+++ homebrew.sh
@@ -1,8 +1,49 @@
 #!/usr/bin/env sh
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed
+
+# Install GnuPG to enable PGP-signing commits.
+brew install gnupg
+
+# Install more recent versions of some macOS tools.
+brew install vim --with-override-system-vi
+brew install grep
+brew install openssh
 
 # Install generally required packages
-brew install openssl@1.1 libevent
-brew install autoconf pkg-config readline sqlite3 xz zlib
+brew install openssl
+brew install libevent
+brew install autoconf
+brew install pkg-config
+brew install readline
+brew install sqlite3
+brew install xz
+brew install zlib
+
+# Install generally useful pckages
+brew install ack
+brew install git
+brew install git-lfs
+brew install gs
+brew install pv
+brew install rename
+brew install rlwrap
+brew install ssh-copy-id
+brew install tree
+brew install wget
 
